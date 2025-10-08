b3989770a096a29bff57b9c954cad9ef9da5cd12
fourk9 <andreyaratha665@gmail.com>
2025-10-07 19:13:03 -0600

    Se actualiza calculadora con código básico

--- src/CalculadoraBasica.java
+++ src/CalculadoraBasica.java
@@ -1,12 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
  * @author arath
  */
 public class CalculadoraBasica {
-    
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        return a / b;
+    }
+
 }

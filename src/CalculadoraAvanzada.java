ab117316e33222954a34eea3548d5763eb6622db
fourk9 <andreyaratha665@gmail.com>
2025-10-07 19:24:59 -0600

    Se crea la calculadora avanzada y se arregla problema en la división

--- src/CalculadoraAvanzada.java
+++ src/CalculadoraAvanzada.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
  * @author arath
  */
-public class CalculadoraAvanzada {
-    
+public class CalculadoraAvanzada extends CalculadoraBasica {
+
+    public double potencia(int base, int exponente) {
+        return Math.pow(base, exponente);
+    }
+
 }
--- src/CalculadoraBasica.java
+++ src/CalculadoraBasica.java
@@ -17,8 +17,20 @@ public class CalculadoraBasica {
         return a * b;
     }
 
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
     public int dividir(int a, int b) {
         return a / b;
+    }
+
+    public double dividir(double a, double b) {
+        double resultado = 0;
+        if (b != 0) {
+            resultado = a / b;
+        }
+        return resultado;
     }
 
 }
